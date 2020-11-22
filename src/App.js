b77a0a4332bbdc090aa5e01db02510cba9243a37
Claudio Kirchhoff <claudio.kirchhoff@gmail.com>
2020-11-22 06:33:03 +0100

    change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          This line was changed!
         </a>
       </header>
     </div>
